19ac415dc297ea5a3abf6f24672f757aefa9f663
Muhammad Dhafin Fauzan <muhammad.dhafin01@ui.ac.id>
2023-05-23 01:54:48 +0700

    Dafin send message

---
+++ broadcast-service/controllers/broadcastController.js
@@ -0,0 +1,44 @@
+const broadcast = require("../models/broadcastModel");
+const axios = require('axios');
+
+module.exports.getBroadcastMessage = async (req, res, next) => {
+  try {
+    const { from,from_username, message } = req.body;
+
+    const getAllContact = async () => {
+        const response = await axios.get(`http://34.28.228.30/api/contact/${from_username}`);
+        const data = await response.data;
+        return data.map(item => item.contact_id); 
+      };
+
+    const contactList = await getAllContact();
+
+    contactList.forEach(contact => {
+        const handleSendMessage = async ()=>{
+          const sendMessage = async () => {
+            const response = await axios.post(`http://34.135.219.176:8888/api/message/addmsg`,{
+            from: from,
+            to: contact,
+            message: message,
+            });
+            const data = await response.data;
+          };
+
+        const send = await sendMessage();
+        };
+      const msg = handleSendMessage();
+    });
+    
+
+    const data = await broadcast.create({
+      message: { text: message },
+      receivers: contactList,
+      sender: from,
+    });
+
+    if (data) return res.json({ msg: "Broadcast successfully." });
+    else return res.json({ msg: "Failed to broadcast" });
+  } catch (ex) {
+    next(ex);
+  }
+};
